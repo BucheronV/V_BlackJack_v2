a83269811efb12c1e1fedbb50be2e7f7f3cd1ef1
BucheronV <dhapvirgil1401@gmail.com>
2024-07-29 09:11:47 +0200

    debut de semaine

---
+++ ReadMe.txt
@@ -0,0 +1,3 @@
+si j'ai un peu de temps on passera en côté back-end avec une possibilé d'avoir un compte et des jetons pour pouvoir jouer au mini-jeux
+une navbar gérer en back-end/JS
+le plus important reste le JS du mini-jeux
--- script/Game.js
+++ script/Game.js
@@ -26,10 +26,15 @@ export function blackJack () {
         PENSE BÊTE ^_^ ET PRÉPARATION DU CODE 
         faire l'action btn start-game
         faire l'action btn nouvelle carte
+        ->apporter une carte dans le tableau de la main du joueur à chaque fois que le joueur appuis sur le boutton afin de se rapprocher du nombre 21
+
         action btn se coucher (accepter de perdre)
+        -> si le joueur s'apperçoit que son score est au dessus de 21 il a la possibilité de se coucher et de donner la victoire à la banque directement sans passer par le show
+        --> addEnvelistener end game car en vrai le joueur a le choit de pouvoir continuer ou non même si il est dessus du nombre 21
+
+        btn "show"
         le calcul des cartes
         comparatif entre la main du joueur et de la banque
-
         si et seulement ci le joueur appui sur le boutton "show" sinon on fait les boucles vu au dessus
         -> voir si le joueur est au dessus de 21
         -->if(joueur>21){
